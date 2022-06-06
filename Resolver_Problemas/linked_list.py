f3b6ce722eff05cd7a67a3dc7f86eaf9c350805c
Nguyentranquocthang <thangv3009@gmail.com>
2022-06-07 00:16:10 +0700

    Pascal Triangle

--- Resolver_Problemas/linked_list.py
+++ Resolver_Problemas/linked_list.py
@@ -671,38 +671,42 @@ The Idea:
 4. Return max_sum
 '''
 
+
 def max_sum_subarray(arr):
-    
-    current_sum = arr[0] # current_sum denotes the sum of a subarray
-    max_sum = arr[0]     # max_sum denotes the maximum value of current_sum ever
-    
+
+    current_sum = arr[0]  # current_sum denotes the sum of a subarray
+    # max_sum denotes the maximum value of current_sum ever
+    max_sum = arr[0]
+
     # Loop from VALUE at index position 1 till the end of the array
     for element in arr[1:]:
-        
+
         '''
         # Compare (current_sum + element) vs (element)
         # If (current_sum + element) is higher, it denotes the addition of the element to the current subarray
         # If (element) alone is higher, it denotes the starting of a new subarray
         '''
         current_sum = max(current_sum + element, element)
-        
+
         # Update (overwrite) max_sum, if it is lower than the updated current_sum
-        max_sum = max(current_sum, max_sum)   
-    
+        max_sum = max(current_sum, max_sum)
+
     return max_sum
+
 
 def test_function(test_case):
     arr = test_case[0]
     solution = test_case[1]
-    
+
     output = max_sum_subarray(arr)
     if output == solution:
         print("Pass")
     else:
         print("Fail")
 
-arr= [1, 2, 3, -4, 6]
-solution= 8 # sum of array
+
+arr = [1, 2, 3, -4, 6]
+solution = 8  # sum of array
 
 test_case = [arr, solution]
 test_function(test_case)
@@ -715,4 +719,101 @@ arr = [-12, 15, -13, 14, -1, 2, 1, -5, 4]
 solution = 18  # sum of subarray = [15, -13, 14, -1, 2, 1]
 
 test_case = [arr, solution]
+test_function(test_case)
+
+# ---------------------------------------- Pascal Triangle  ----------------------------------------
+print("------------------------ Pascal Triangle ----------------------------")
+
+
+# Solution
+
+'''
+Points to note:
+1. We have to return a list.
+2. The elements of n^th row are made up of elements of (n-1)^th row. This comes up till the 1^st row. We will follow a top-down approach. 
+3. Except for the first and last element, any other element at position j in the current row is the sum of elements at position j and j-1 in the previous row. 
+4. Be careful about the edge cases, example, an index should never be a NEGATIVE at any point of time. 
+'''
+
+
+# def nth_row_pascal(n):
+
+#     if n == 0:
+#         return [1]
+
+#     current_row = [1]  # First row
+
+#     ''' Loop from 1 to n; i denotes the row number'''
+#     for i in range(1, n + 1):
+#         # Set the current_row from previous iteration as the previous_row
+#         previous_row = current_row
+
+#         # Let's build the fresh current_row gradually
+#         # add the default first element at the 0^th index of the i^th row
+#         current_row = [1]
+
+#         '''Loop from 1 to (i-1); j denotes the index of an element with in the i^th row'''
+#         # Example, for 5th row we have considered n=4,
+#         # we will iterate index from 1 to 3, because
+#         # the default element at the 0^th index has already been added
+#         for j in range(1, i):
+
+#             # An element at position j in the current row is the
+#             # sum of elements at position j and j-1 in the previous row.
+#             next_number = previous_row[j] + previous_row[j - 1]
+
+#             # Append the new element to the current_row
+#             current_row.append(next_number)
+
+#         current_row.append(1)  # append the default last element
+#     return current_row
+
+def nth_row_pascal(n):
+    if n == 0:
+        return [1]
+    current_row = [1]
+    for i in range(1, n + 1):
+        previous_row = current_row
+        current_row = [1]
+        for j in range(1, i):
+            number = previous_row[j] + previous_row[j-1]
+            current_row.append(number)
+        current_row.append(1)
+    return current_row
+
+
+def test_function(test_case):
+    n = test_case[0]
+    solution = test_case[1]
+    output = nth_row_pascal(n)
+    if solution == output:
+        print("Pass")
+    else:
+        print("Fail")
+
+
+n = 0
+solution = [1]
+
+test_case = [n, solution]
+test_function(test_case)
+n = 1
+solution = [1, 1]
+
+test_case = [n, solution]
+test_function(test_case)
+n = 2
+solution = [1, 2, 1]
+
+test_case = [n, solution]
+test_function(test_case)
+n = 3
+solution = [1, 3, 3, 1]
+
+test_case = [n, solution]
+test_function(test_case)
+n = 4
+solution = [1, 4, 6, 4, 1]
+
+test_case = [n, solution]
 test_function(test_case)
